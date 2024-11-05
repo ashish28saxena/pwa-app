074515ecab29e51c39b7ed911160989192891b2a
ashish saxena <ashish28saxena@gmail.com>
2024-11-06 00:35:05 +0530

    this is secand commit changes inside userlist

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,24 @@
-console.log("sw file is public folder")
+let cacheData="appV1";
+this.addEventListener("install",(event)=>{
+ event.waitUntil(
+    caches.open(cacheData).then((cache)=>{
+        cache.addAll([
+            '/static/js/bundle.js',
+            
+            '/index.html',
+            '/'
+            
+        ])
+    })
+ )
+})
+this.addEventListener("fetch",(event)=>{
+        event.respondWith(
+            caches.match(event.request).then((resp)=>{
+                if(resp)
+                {
+                    return resp;
+                }
+            })
+        )
+})
--- src/component/User.js
+++ src/component/User.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 const User=()=>{
+    const [getdata , setGetdata]=useState([]);
+    
+    useEffect(() => {
+        fetch('https://dummyjson.com/users')
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            let a=data;
+            console.log('&&&&&&&&&',a); 
+            setGetdata(a);
+            console.log('#######',getdata); 
+            
+          });
+      },[]);
     return (
         <div><h1>this is user component </h1></div>
     )
