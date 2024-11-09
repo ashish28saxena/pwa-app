c3b3d697d9f0ba61da2778ba86eefa72fd99a7ed
ashish saxena <ashish28saxena@gmail.com>
2024-11-09 13:26:21 +0530

    code commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import Nav from './component/Nav';
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Nav />);
+  const linkElement = screen.getByText(/this is app component/i);
   expect(linkElement).toBeInTheDocument();
 });
